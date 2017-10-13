4ee63da302d1014db44fbdf76dce39d373f1e1b0
Boriclav <32224254+Boriclav@users.noreply.github.com>
2017-10-13 19:15:40 +0300

    Update chance.py

--- chance.py
+++ chance.py
@@ -1,12 +1,14 @@
 # Напишите программу проверяющую является ли число составным.
 # В программе гарантируется, что число - натуральное.
 n = int(input())
+a = 0
+if n == 1:
+	print('Unit')
 if n>1:
-	for i in range(2,n):
+	for i in range(2, n):
 		if (n%i) == 0:
 			print(n,'is Composite')
+			a = a + 1
 			break
-		else:
-			print(n,'is Prime')
-else:
-	 print(1,'is Unit')
+if (a != 1) and (n != 1):
+			print(n, 'is prime')
