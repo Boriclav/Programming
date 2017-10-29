0a1d562f83e382183dd8bda888779365ae324c87
Boriclav <32224254+Boriclav@users.noreply.github.com>
2017-10-29 19:57:29 +0300

    Update massiv2.py

--- massiv2.py
+++ massiv2.py
@@ -4,13 +4,18 @@ A = []
 n = int(input())
 min = 0
 max = 0
-sum = 0 
+sum = 0
+avg = float
 for i in range(n):
 	A.append(int(input()))
 	if A[i]<min:
 		min = A[i]
+	if(i==0):
+		min=A[i]
+		min=A[i]
 	if A[i]>max:
 		max = A[i]
 	sum = sum + A[i]
+	avg= sum/len(A)
 print(A)
-print ('min=',min,'max=',max,'avg=', sum/len(A))
+print ('min=',min,'max=',max,'avg=', avg)
